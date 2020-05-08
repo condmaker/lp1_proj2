f6c8039f21bf339c897fff1805782dd63ab0a4f8
Daniel Fernandes <danielteotonio@sapo.pt>
2020-05-08 20:04:45 +0100

    Add player turn functionality in Board.cs

--- Felli/Board.cs
+++ Felli/Board.cs
@@ -7,6 +7,71 @@ namespace Felli
     /// </summary>
     public class Board
     {
+        public Tilestate[] board;
+        private int turn;
+        private Tilestate firstPlayer, secondPlayer;
+
+        //Property that returns the player that is currently playing 
+        public Tilestate NextTurn
+        {
+            get
+            {
+                if(GameOver)
+                {
+                    return Tilestate.Empty;
+                }
+                else if(turn % 2 == 0)
+                {
+                    return firstPlayer;
+                }
+                else
+                {
+                    return secondPlayer;
+                }
+
+            }
+        }
+
+        //Property that checks if the game is over or not
+        public bool GameOver
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public Board()
+        {
+            board = new Tilestate[13];
+            turn = 0;
+        }
+
+        //Selects the players turns
+        public void SelectPlayersTurn(Tilestate firstPlayer)
+        {
+            
+            if(firstPlayer == Tilestate.Black)
+            {
+                //Selects Black to be the first player to play
+                this.firstPlayer = firstPlayer;     
+                secondPlayer = Tilestate.White;         
+            }
+            else if(firstPlayer == Tilestate.Black)
+            {
+                //Selects White to be the first player to play
+                this.firstPlayer = firstPlayer;     
+                secondPlayer = Tilestate.White;        
+            }
+            else
+            {
+                //Error message probably
+            }
+
+
+        }
         
+
+
     }
 }
